6ba50443ab75645579aa51ccc7d70d53b76b7a1e
Wither <deonvader55@gmail.com>
2024-12-28 18:34:31 +0100

    Add's Broken Blocks

--- src/main/java/net/wither/new_in_old/block/ModBlocks.java
+++ src/main/java/net/wither/new_in_old/block/ModBlocks.java
@@ -2,9 +2,7 @@ package net.wither.new_in_old.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
-import net.minecraft.block.PillarBlock;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
@@ -21,7 +19,22 @@ public class ModBlocks {
             new PillarBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
     public static final Block BAMBOO_PLANKS = registerBlock("bamboo_planks",
             new Block(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
-
+    public static final Block BAMBOO_BUTTON = registerBlock("bamboo_button",
+            new WoodenButtonBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+    public static final Block BAMBOO_DOOR = registerBlock("bamboo_door",
+            new DoorBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+    public static final Block BAMBOO_FENCE = registerBlock("bamboo_fence",
+            new FenceBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+    public static final Block BAMBOO_FENCE_GATE = registerBlock("bamboo_fence_gate",
+            new FenceGateBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+    public static final Block BAMBOO_MOSAIC = registerBlock("bamboo_mosaic",
+            new Block(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+    public static final Block BAMBOO_MOSAIC_SLAB = registerBlock("bamboo_mosaic_slab",
+            new SlabBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+    public static final Block BAMBOO_PRESSSURE_PLATE = registerBlock("bamboo_pressure_plate",
+            new Block(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+    public static final Block BAMBOO_SLAB = registerBlock("bamboo_slab",
+            new SlabBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
 
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
