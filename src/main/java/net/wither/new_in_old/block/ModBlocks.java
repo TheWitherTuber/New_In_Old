e24f190b54712874017e672f3c4ec283c56ff9f6
Wither <deonvader55@gmail.com>
2024-12-28 20:48:27 +0100

    TRAPDOORS ACTUALLY WORK NOW

--- src/main/java/net/wither/new_in_old/block/ModBlocks.java
+++ src/main/java/net/wither/new_in_old/block/ModBlocks.java
@@ -14,29 +14,29 @@ import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
     public static final Block BLOCK_OF_BAMBOO = registerBlock("block_of_bamboo",
-            new PillarBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+            new PillarBlock(FabricBlockSettings.of(Material.BAMBOO_SAPLING)), ItemGroup.BUILDING_BLOCKS);
     public static final Block STRIPPED_BLOCK_OF_BAMBOO = registerBlock("stripped_block_of_bamboo",
-            new PillarBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+            new PillarBlock(FabricBlockSettings.of(Material.BAMBOO_SAPLING)), ItemGroup.BUILDING_BLOCKS);
     public static final Block BAMBOO_PLANKS = registerBlock("bamboo_planks",
-            new Block(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+            new Block(FabricBlockSettings.of(Material.BAMBOO_SAPLING)), ItemGroup.BUILDING_BLOCKS);
     public static final Block BAMBOO_BUTTON = registerBlock("bamboo_button",
-            new WoodenButtonBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+            new WoodenButtonBlock(FabricBlockSettings.of(Material.BAMBOO_SAPLING)), ItemGroup.BUILDING_BLOCKS);
     public static final Block BAMBOO_DOOR = registerBlock("bamboo_door",
-            new DoorBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+            new DoorBlock(FabricBlockSettings.of(Material.BAMBOO_SAPLING)), ItemGroup.BUILDING_BLOCKS);
     public static final Block BAMBOO_FENCE = registerBlock("bamboo_fence",
-            new FenceBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+            new FenceBlock(FabricBlockSettings.of(Material.BAMBOO_SAPLING)), ItemGroup.BUILDING_BLOCKS);
     public static final Block BAMBOO_FENCE_GATE = registerBlock("bamboo_fence_gate",
-            new FenceGateBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+            new FenceGateBlock(FabricBlockSettings.of(Material.BAMBOO_SAPLING)), ItemGroup.BUILDING_BLOCKS);
     public static final Block BAMBOO_MOSAIC = registerBlock("bamboo_mosaic",
-            new Block(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+            new Block(FabricBlockSettings.of(Material.BAMBOO_SAPLING)), ItemGroup.BUILDING_BLOCKS);
     public static final Block BAMBOO_MOSAIC_SLAB = registerBlock("bamboo_mosaic_slab",
-            new SlabBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+            new SlabBlock(FabricBlockSettings.of(Material.BAMBOO_SAPLING)), ItemGroup.BUILDING_BLOCKS);
     //public static final Block BAMBOO_PRESSSURE_PLATE = registerBlock("bamboo_pressure_plate",
             //new PressurePlateBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
     public static final Block BAMBOO_SLAB = registerBlock("bamboo_slab",
-            new SlabBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+            new SlabBlock(FabricBlockSettings.of(Material.BAMBOO_SAPLING)), ItemGroup.BUILDING_BLOCKS);
     public static final Block BAMBOO_TRAPDOOR = registerBlock("bamboo_trapdoor",
-            new TrapdoorBlock(FabricBlockSettings.of(Material.BAMBOO)), ItemGroup.BUILDING_BLOCKS);
+            new TrapdoorBlock(FabricBlockSettings.of(Material.BAMBOO_SAPLING).nonOpaque()), ItemGroup.BUILDING_BLOCKS);
 
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
--- src/main/java/net/wither/new_in_old/new_in_oldClient.java
+++ src/main/java/net/wither/new_in_old/new_in_oldClient.java
@@ -2,11 +2,12 @@ package net.wither.new_in_old;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 import net.wither.new_in_old.block.ModBlocks;
 
 public class new_in_oldClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.BAMBOO_TRAPDOOR, RenderLayer.getCutout());
     }
 }
