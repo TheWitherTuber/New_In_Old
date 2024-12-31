d49f538e10f7da47065df4ac2bc6efd3c7168d75
Wither <deonvader55@gmail.com>
2024-12-31 18:02:29 +0100

    Server branch

--- src/main/java/net/wither/new_in_old/util/ModStrippableBlocks.java
+++ src/main/java/net/wither/new_in_old/util/ModStrippableBlocks.java
@@ -4,7 +4,7 @@ import net.fabricmc.fabric.api.registry.StrippableBlockRegistry;
 import net.wither.new_in_old.block.ModBlocks;
 
 public class ModStrippableBlocks {
-    public static void registerStrippables(){
+    public static void registerStrippables() {
         StrippableBlockRegistry.register(ModBlocks.BLOCK_OF_BAMBOO, ModBlocks.STRIPPED_BLOCK_OF_BAMBOO);
         StrippableBlockRegistry.register(ModBlocks.CHERRY_LOG, ModBlocks.STRIPPED_CHERRY_LOG);
         StrippableBlockRegistry.register(ModBlocks.CHERRY_WOOD, ModBlocks.STRIPPED_CHERRY_WOOD);
